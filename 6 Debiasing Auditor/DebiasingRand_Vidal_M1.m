09f670cac16698365a363000302cdac18ddb2f49
AuditingBio <60875185+AuditingBio@users.noreply.github.com>
2020-02-10 05:02:11 -0500

    Add files via upload

---
+++ 6 Debiasing Auditor/DebiasingRand_Vidal_M1.m
@@ -0,0 +1,148 @@
+%DebiasingRand_Vidal_M1
+% Process: Debiasing Auditor
+% Data: Vidal dataset
+% Framework: M1 (F1)
+% Manipulation: Balancing node degree bias in training and masking features
+% with random numbers (method: r2)
+%02.15.2019
+
+
+clear
+training_rounds = 10;
+
+%% Add data and function paths
+crnt_dir = pwd;
+parent_dir = crnt_dir(1:(find(crnt_dir==crnt_dir(1),1,'last'))-1);
+addpath(genpath(parent_dir))
+
+%% Training and testing data
+%load VidalHH.mat
+load VidalHH_RedRed.mat %sequence redundancy reduced
+
+%% Balanced negative training data
+NEGtr = NEGtrB;
+
+%% Output
+TKs='05';
+TaskTitle='DebiasRand';
+Model='M1';
+Dataset='VidalHH';
+OutputFileHeader=['TK',TKs,'_',TaskTitle,'_', Model,'_', Dataset]
+SumFile=['TK', num2str(TKs),'Summary','_',TaskTitle,'_', Model,'_', Dataset]
+
+%% Parameter search space
+Cs=[100,10, 1]';
+Gammas=[0.1,0.01,0.001,0.0001,0.00001]';
+Paramters=[];
+for c=1:length(Cs)
+    Paramters=[Paramters;repmat(Cs(c),size(Gammas)),Gammas];
+end
+
+%% Features
+load Features_M1_VidalHH_r2
+
+%% Parameter optimization rounds
+for paramterSet=1:length(Paramters)
+    paramterSet
+    
+    crntC = Paramters(paramterSet,1);
+    crntGamma = Paramters(paramterSet,2);
+    ParVector=[' -c ',num2str(crntC),'  -t 4 -h 0 -m 10000' ];
+    
+    OutputFile=[OutputFileHeader,'_', num2str(paramterSet)  , '.mat'];
+    
+    %% Training rounds
+    ESTIMATES_Tr = cell(training_rounds,1);
+    Stat_Tr = zeros(training_rounds,7);
+    ESTIMATES_TsCV = cell(training_rounds,1);
+    Stat_CV = zeros(training_rounds,7);
+    
+    ESTIMATES_TsC1 = cell(training_rounds,1);
+    Stat_C1 = zeros(training_rounds,7);
+    ESTIMATES_TsC2 = cell(training_rounds,1);
+    Stat_C2 = zeros(training_rounds,7);
+    ESTIMATES_TsC3 = cell(training_rounds,1);
+    Stat_C3 = zeros(training_rounds,7);
+    
+    
+    SVr = zeros(training_rounds,1);
+    TIMEs = zeros(training_rounds,1);
+    
+    for k=1:training_rounds
+        k
+        tic
+        
+        %% Randomized features
+        DotSign = DotSign_Rand{k};
+        
+        %% Training and validation sets
+        postr=POStr{k};
+        negtr=NEGtr{k};
+        poscv=POSCV{k};
+        negcv=NEGCV{k};
+        
+        Tr=[postr;negtr];
+        CV=[poscv;negcv];
+        
+        %% Test sets
+        posc1 = POStsC1{k};
+        posc2 = POStsC2{k};
+        posc3 = POStsC4{k};
+        negc1 = NEGtsC1{k};
+        negc2 = NEGtsC2{k};
+        negc3 = NEGtsC4{k};
+        
+        C1 = [posc1;negc1];
+        C2 = [posc2;negc2];
+        C3 = [posc3;negc3];
+        
+        %% Precomputed Kernel
+        %M1 Kernel: Symmteric+ normalize for length+ basic singature+ Gaussianized
+        StTrain  = fun_Kernel_M1(Tr,Tr,DotSign,crntGamma);
+        StCV     = fun_Kernel_M1(CV,Tr,DotSign,crntGamma);
+        
+        StC1    = fun_Kernel_M1(C1,Tr,DotSign,crntGamma);
+        StC2    = fun_Kernel_M1(C2,Tr,DotSign,crntGamma);
+        StC3    = fun_Kernel_M1(C3,Tr,DotSign,crntGamma);
+        
+        %% Train
+        PosLabel = 1;
+        NegLabel = -1;
+        posclass = PosLabel;
+        
+        TrLabel = [PosLabel*ones(length(postr),1); NegLabel*ones(length(negtr),1)];
+        if isequal(computer,'MACI64')
+            MODEL=svmtrainXX(TrLabel,double(StTrain), ParVector); %mexa file for mac 64bit
+        else
+            MODEL=svmtrainX(TrLabel,double(StTrain), ParVector);  %assumed linux
+            % If running on other operating system, mexa files from LibSVM
+            % should be generated. Download LibSVM for Matlab from LibSVM
+            % website.
+        end
+        SVr(k) = MODEL.totalSV/length(TrLabel);
+        
+        %% Train performance
+        [ESTIMATES_Tr{k},Stat_Tr(k,:),~,~,~,~] = fun_STDsvmTest1(MODEL,postr,negtr,StTrain,PosLabel,NegLabel,posclass);
+        
+        %% CV performance
+        [ESTIMATES_TsCV{k},Stat_CV(k,:),~,~,~,~ ] = fun_STDsvmTest1(MODEL,poscv,negcv,StCV,PosLabel,NegLabel,posclass);
+        
+        %% Test performance
+        [ESTIMATES_TsC1{k},Stat_C1(k,:),~,~,~,~ ] = fun_STDsvmTest1(MODEL,posc1,negc1,StC1,PosLabel,NegLabel,posclass);
+        [ESTIMATES_TsC2{k},Stat_C2(k,:),~,~,~,~ ] = fun_STDsvmTest1(MODEL,posc2,negc2,StC2,PosLabel,NegLabel,posclass);
+        [ESTIMATES_TsC3{k},Stat_C3(k,:),~,~,~,~ ] = fun_STDsvmTest1(MODEL,posc3,negc3,StC3,PosLabel,NegLabel,posclass);
+        
+        
+        %% Save results
+        TIMEs(k,1)=toc;
+        toc
+        
+        save(OutputFile,'crntC','crntGamma','-regexp','^ESTIMATES_','^Stat_',...
+            'TIMEs','SVr','ParVector','-v7.3')
+        
+    end
+end
+
+%% Summary stats
+[Summary , SummaryAUC] = fun_SummaryStatFiles( Paramters, [], [], ...
+    OutputFileHeader, SumFile, []);
---
+++ 6 Debiasing Auditor/DebiasingRand_Vidal_M7symmetric.m
@@ -0,0 +1,211 @@
+%DebiasingRand_Vidal_M7symmetric
+% Process: Debiasing Auditor
+% Data: Vidal dataset
+% Framework: M7 (F7)
+% Manipulation: Balancing node degree bias in training, masking features
+% with random numbers (method: r2), and accounting for learning asymmetricity
+%01.09.2019
+
+
+clear
+training_rounds = 10;
+GlobalTime = tic;
+
+%% Add data and function paths
+crnt_dir = pwd;
+parent_dir = crnt_dir(1:(find(crnt_dir==crnt_dir(1),1,'last'))-1);
+addpath(genpath(parent_dir))
+
+%% Training and testing data
+%load VidalHH.mat
+load VidalHH_RedRed.mat %sequence redundancy reduced
+
+%% Balanced negative training data
+NEGtr = NEGtrB;
+
+%% Output
+TKs='05';
+TaskTitle='DebiasRand';
+Model='M7';
+Dataset='VidalHHSymm';
+OutputFileHeader=['TK',TKs,'_',TaskTitle,'_', Model,'_', Dataset]
+SumFile=['TK', num2str(TKs),'Summary','_',TaskTitle,'_', Model,'_', Dataset]
+
+%% Paramters
+NegLabel = 0;
+TH = 0.5;
+MaxEpochs1 = 700;
+MaxEpochs3 = 300;
+
+%% Parameter search space
+hiddenSize  = [100:100:700]';
+Regularize = [0.001, 0.005, 0.01, 0.05, 0.1]';
+Paramters=[];
+for c=1:length(hiddenSize)
+    Paramters=[Paramters;repmat(hiddenSize(c),size(Regularize)),Regularize];
+end
+ParameterSelection = [1:5, 16:20];
+
+%% Features
+load Features_M3_VidalHH_r2 Features_Rand
+
+%% Parameter optimization rounds
+for paramterSet=ParameterSelection
+    paramterSet
+    
+    crntHiddenSize = Paramters(paramterSet,1);
+    crntRegulariz = Paramters(paramterSet,2);
+    
+    OutputFile=[OutputFileHeader,'_', num2str(paramterSet)  , '.mat'];
+    
+    %% Training rounds
+    ESTIMATES_Tr = cell(training_rounds,1);
+    Stat_Tr = zeros(training_rounds,7);
+    ESTIMATES_TsCV = cell(training_rounds,1);
+    Stat_CV = zeros(training_rounds,7);
+    SVr = zeros(training_rounds,1);
+    TIMEs = zeros(training_rounds,1);
+    
+    ESTIMATES_TsC1 = cell(training_rounds,1);
+    Stat_C1 = zeros(training_rounds,7);
+    ESTIMATES_TsC2 = cell(training_rounds,1);
+    Stat_C2 = zeros(training_rounds,7);
+    ESTIMATES_TsC3 = cell(training_rounds,1);
+    Stat_C3 = zeros(training_rounds,7);
+    
+    for k=1:training_rounds
+        k
+        tic
+        MajorT = tic;
+        
+        %% Randomized features
+        Features = Features_Rand{k};
+        
+        %% Symmetric training sets
+        postr=[POStr{k};POStr{k}(:,2),POStr{k}(:,1)];
+        negtr=[NEGtr{k};NEGtr{k}(:,2),NEGtr{k}(:,1)];
+        Tr=[postr;negtr];
+        
+        %% Validation sets
+        poscv=POSCV{k};
+        negcv=NEGCV{k};
+        CV=[poscv;negcv];
+        
+        %% Test sets
+        posc1 = POStsC1{k};
+        posc2 = POStsC2{k};
+        posc3 = POStsC4{k};
+        negc1 = NEGtsC1{k};
+        negc2 = NEGtsC2{k};
+        negc3 = NEGtsC4{k};
+        
+        C1 = [posc1;negc1];
+        C2 = [posc2;negc2];
+        C3 = [posc3;negc3];
+        
+        %% Prepare training and testing matrices
+        CrntPosMatTr=[Features(postr(:,1),:),...
+            Features(postr(:,2),:)];
+        CrntNegMatTr=[Features(negtr(:,1),:),...
+            Features(negtr(:,2),:)];
+        
+        CrntPosMatCV=[Features(poscv(:,1),:),...
+            Features(poscv(:,2),:)];
+        CrntNegMatCV=[Features(negcv(:,1),:),...
+            Features(negcv(:,2),:)];
+        
+        % Standardized
+        % [StTrain,StCV,~,~] = fun_Standarize_TrTs_MSD(...
+        %     [CrntPosMatTr;CrntNegMatTr],[CrntPosMatCV;CrntNegMatCV]);
+        
+        % Non-standardized
+        StTrain=[CrntPosMatTr;CrntNegMatTr];
+        StCV=[CrntPosMatCV;CrntNegMatCV];
+        
+        %C1
+        CrntPosMatC1=[Features(posc1(:,1),:),...
+            Features(posc1(:,2),:)];
+        CrntNegMatC1=[Features(negc1(:,1),:),...
+            Features(negc1(:,2),:)];
+        StC1 = [CrntPosMatC1;CrntNegMatC1];
+        
+        %C2
+        CrntPosMatC2=[Features(posc2(:,1),:),...
+            Features(posc2(:,2),:)];
+        CrntNegMatC2=[Features(negc2(:,1),:),...
+            Features(negc2(:,2),:)];
+        StC2 = [CrntPosMatC2;CrntNegMatC2];
+        
+        %C4
+        CrntPosMatC3=[Features(posc3(:,1),:),...
+            Features(posc3(:,2),:)];
+        CrntNegMatC3=[Features(negc3(:,1),:),...
+            Features(negc3(:,2),:)];
+        StC3 = [CrntPosMatC3;CrntNegMatC3];
+        
+        %% Train preparation
+        PosLabel = 1;
+        posclass = PosLabel;
+        
+        TrLabel = [PosLabel*ones(length(postr),1); NegLabel*ones(length(negtr),1)];
+        CVLabel = [PosLabel*ones(length(poscv),1); NegLabel*ones(length(negcv),1)];
+        
+        %% Moodel
+        % Autoencoder
+        rng('default')
+        T= tic;
+        autoenc1 = trainAutoencoder(StTrain',crntHiddenSize,...
+            'MaxEpochs',MaxEpochs1, ...
+            'L2WeightRegularization',crntRegulariz, ...
+            'ScaleData', false);
+        fprintf('Training Autoencoder 1: %d sec. \n',toc(T))
+        
+        % Input toSoftmax
+        feat1 = encode(autoenc1,StTrain');
+        
+        % 1-layer model
+        T = tic;
+        softnet= trainSoftmaxLayer(feat1,TrLabel','MaxEpochs',MaxEpochs3);
+        deepnet1 = stack(autoenc1,softnet);
+        
+        %% Train
+        deepnet2 = train(deepnet1,StTrain',TrLabel');
+        fprintf('Training the fine-tuned deepnet: %d sec. \n',toc(T))
+        
+        %% Train performance
+        ESTIMATES_Tr{k} = deepnet2(StTrain');
+        Stat_Tr(k,:) = fun_Stats_Prediction2(TrLabel,ESTIMATES_Tr{k},PosLabel,NegLabel,posclass,TH)
+        
+        %% CV performance
+        ESTIMATES_TsCV{k} = deepnet2(StCV');
+        Stat_CV(k,:)= fun_Stats_Prediction2(CVLabel,ESTIMATES_TsCV{k},PosLabel,NegLabel,posclass,TH)
+        
+        %% Test performance
+        C1Label = [PosLabel*ones(length(posc1),1); NegLabel*ones(length(negc1),1)];
+        C2Label = [PosLabel*ones(length(posc2),1); NegLabel*ones(length(negc2),1)];
+        C3Label = [PosLabel*ones(length(posc3),1); NegLabel*ones(length(negc3),1)];
+        
+        ESTIMATES_TsC1{k} = deepnet2(StC1');
+        Stat_C1(k,:)= fun_Stats_Prediction2(C1Label,ESTIMATES_TsC1{k},PosLabel,NegLabel,posclass,TH)
+        
+        ESTIMATES_TsC2{k} = deepnet2(StC2');
+        Stat_C2(k,:)= fun_Stats_Prediction2(C2Label,ESTIMATES_TsC2{k},PosLabel,NegLabel,posclass,TH)
+        
+        ESTIMATES_TsC3{k} = deepnet2(StC3');
+        Stat_C3(k,:)= fun_Stats_Prediction2(C3Label,ESTIMATES_TsC3{k},PosLabel,NegLabel,posclass,TH)
+        
+        %% Save results
+        toc(MajorT)
+        TIMEs(k,1) = toc(MajorT);
+        
+        save(OutputFile,'crntHiddenSize','crntRegulariz','-regexp','^ESTIMATES_','^Stat_',...
+            'TIMEs','-v7.3')
+        
+    end
+end
+
+%% Summary stats
+GlobalTimeAmount = toc(GlobalTime)
+
+[Summary , SummaryAUC] = fun_SummaryStatFiles( Paramters, [], [], ...
+    OutputFileHeader, SumFile, []);
